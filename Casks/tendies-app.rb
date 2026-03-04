37f0c81dbc051fb73b67fa04b2ca378ee1494915
Batjaa Batbold <batjaa0615@gmail.com>
2026-03-04 14:10:24 -0800

    feat: add tendies-app cask (placeholder for first release)

---
+++ Casks/tendies-app.rb
@@ -0,0 +1,11 @@
+cask "tendies-app" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/batjaa/tendies-app/releases/download/v#{version}/TendiesApp.zip"
+  name "TendiesApp"
+  desc "macOS menu bar app for tracking day trading P&L"
+  homepage "https://github.com/batjaa/tendies-app"
+
+  app "TendiesApp.app"
+end
